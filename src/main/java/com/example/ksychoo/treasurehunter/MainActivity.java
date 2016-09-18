b6458864919110971b15c89dd54227241a20a704
Vsmasster <krasnal1995@gmail.com>
2016-09-18 12:41:48 +0200

    Display distance to treasure location

--- src/main/java/com/example/ksychoo/treasurehunter/MainActivity.java
+++ src/main/java/com/example/ksychoo/treasurehunter/MainActivity.java
@@ -1,13 +1,127 @@
 package com.example.ksychoo.treasurehunter;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    TextView txtDistanceValue;
+    private static Location targetLocation;
+
+    public static GoogleApiClient mGoogleApiClient;
+
+    private static LocationRequest mLocationRequest;
+
+    private static final String[] LOCATION_PERMS = {
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.INTERNET
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtDistanceValue = (TextView) findViewById(R.id.txtDistanceValue);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(LOCATION_PERMS, 1339);
+        }
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        targetLocation = new Location("");
+        targetLocation.setLatitude(50.175266);
+        targetLocation.setLongitude(19.895879);
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(500);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        float distance = location.distanceTo(targetLocation);
+
+        if (distance > 2000) {
+            txtDistanceValue.setText("Very far");
+        } else if (distance > 1000) {
+            txtDistanceValue.setText("Far");
+        } else if (distance > 100) {
+            String text = "";
+
+            for (int i = 1; i < distance; i += 100) {
+                text += "#";
+            }
+
+            txtDistanceValue.setText(text);
+        } else if (distance > 50) {
+            txtDistanceValue.setText("Are you sure?");
+        } else if (distance > 30) {
+            txtDistanceValue.setText("ENEMY AHEAD!!!");
+        } else {
+            txtDistanceValue.setText("You won... " + String.valueOf(distance) +"m");
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
